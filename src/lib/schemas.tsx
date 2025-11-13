fdb2b27ebefefab9d79c925826b5c125eff6d59a
Paul2G <pggaleana@gmail.com>
2025-11-12 21:35:59 -0800

    feat: create schemas and types

---
+++ src/lib/schemas.tsx
@@ -0,0 +1,64 @@
+import { z } from "zod";
+import { phoneNumberPattern } from "@/lib/regex.ts";
+
+export const contactInfoSchema = z.object({
+  fullName: z.string().min(1),
+  emailAddress: z.email().optional(),
+  phoneNumber: z
+    .string()
+    .regex(phoneNumberPattern, { message: "Invalid phone number" })
+    .optional(),
+  address: z.string().optional(),
+  linkedinUrL: z.url().optional(),
+  website: z.url().optional(),
+});
+
+export const experienceItemSchema = z.object({
+  id: z.string(),
+  jobTitle: z.string().min(1),
+  companyName: z.string().min(1),
+  startDate: z.date(),
+  endDate: z.date().optional(),
+  isCurrentlyWorkingHere: z.boolean(),
+  jobDescription: z.string(),
+});
+
+export const educationItemSchema = z.object({
+  id: z.string(),
+  qualification: z.string().min(1),
+  institutionName: z.string().min(1).optional(),
+  location: z.string().optional(),
+  completionDate: z.date().optional(),
+  minor: z.string().optional(),
+  gpa: z.string().optional(),
+  additional: z.string().optional(),
+});
+
+export const projectSchema = z.object({
+  id: z.string(),
+  projectName: z.string().min(1),
+  organization: z.string().optional(),
+  startDate: z.date().optional(),
+  endDate: z.date().optional(),
+  description: z.string().optional(),
+  link: z.url().optional(),
+});
+
+export const certificationSchema = z.object({
+  id: z.string(),
+  certificationName: z.string().min(1),
+  organization: z.string().optional(),
+  issueDate: z.date().optional(),
+  expirationDate: z.date().optional(),
+  credentialId: z.string().optional(),
+  credentialUrl: z.url().optional(),
+  description: z.string().optional(),
+});
+
+export const courseSchema = z.object({
+  id: z.string(),
+  courseName: z.string().min(1),
+  organization: z.string().optional(),
+  completionDate: z.date().optional(),
+  description: z.string().optional(),
+});
---
+++ src/lib/types.tsx
@@ -0,0 +1,44 @@
+import { z } from "zod";
+
+import {
+  certificationSchema,
+  contactInfoSchema,
+  courseSchema,
+  educationItemSchema,
+  experienceItemSchema,
+  projectSchema,
+} from "@/lib/schemas";
+
+export type AppData = {
+  resumes: Resume[];
+  version: string;
+};
+
+export type Resume = {
+  id: string;
+  title: string;
+  summary: string;
+  sectionsOrder: Set<SectionType>;
+  contactInfo: ContactInfo;
+  experience: ExperienceItem[];
+  education: EducationItem[];
+  projects: Project[];
+  certifications: Certification[];
+  courses: Course[];
+  skills: string;
+};
+
+export type ContactInfo = z.infer<typeof contactInfoSchema>;
+export type ExperienceItem = z.infer<typeof experienceItemSchema>;
+export type EducationItem = z.infer<typeof educationItemSchema>;
+export type Project = z.infer<typeof projectSchema>;
+export type Certification = z.infer<typeof certificationSchema>;
+export type Course = z.infer<typeof courseSchema>;
+
+export type SectionType =
+  | "contactInfo"
+  | "experience"
+  | "education"
+  | "projects"
+  | "certifications"
+  | "courses";
