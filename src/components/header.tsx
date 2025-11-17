5e7600abacbd8179e7dd8312baebf6756e86d5fe
Paul2G <pggaleana@gmail.com>
2025-11-16 16:35:57 -0800

    feat: add title to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,11 @@
 import { ModeToggle } from "./mode-toggle";
 
-export function Header({ children }: HeaderProps) {
+export function Header() {
   return (
-    <header className="bg-background sticky top-0 z-50 w-full flex p-2">
+    <header className="bg-background sticky top-0 z-50 w-full flex p-2 border border-b">
+      <h1 className="my-auto text-xl font-bold uppercase italic px-4">
+        Resume Maker
+      </h1>
       <div className="ml-auto">
         <ModeToggle />
       </div>
